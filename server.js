8e5b2a5f4e7aa99d1fde45093bcb3a42d6c18255
AnkitaSahu-4605 <aankisahu0@gmail.com>
2025-09-13 15:42:54 +0530

    firts commit

---
+++ server.js
@@ -0,0 +1,15 @@
+require("dotenv").config()
+const express=require("express")
+const cors=require("cors")
+
+const app=express()
+
+app.use(cors({origin:"*"}))
+
+app.get("/demo",async(req,res)=>{
+    res.status(200).json({message:"app is running"})
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log("SERVER IS RUNNING")
+})
